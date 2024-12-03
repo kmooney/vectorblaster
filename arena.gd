2642e0e01c41fa4dff28a300fe8a1e89c31efb51
Kevin Mooney <kmooney@gmail.com>
2024-12-02 18:10:52 -0600

    sound effect first pass; music first pass

--- arena.gd
+++ arena.gd
@@ -1,9 +1,11 @@
 extends Node3D
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass 
+	var song = load("res://songs/11-30-24-2.mp3")
+	$AudioStreamPlayer.stream = song
+	$AudioStreamPlayer.play()
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
--- weapons/basic_laser.gd
+++ weapons/basic_laser.gd
@@ -1,10 +1,28 @@
 extends Weapon
 class_name BasicLaser
 
+var which_offset = 0
+var offsets =  [0.0, 2.0, 4.0, 6.0, 8.0, 10.0,
+				12.0, 14.0, 16.0, 18.0, 20.0, 22.0]
+@onready var audioplayer = $AudioStreamPlayer
+@onready var timer = $Timer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	fire_rate = 4.0
 	muzzle_velocity = 30.0
 	projectile_scene = preload("res://weapons/laser_projectile.tscn")
+	var laser_sounds = preload("res://sounds/11-30-24-3-lasers.mp3")
+	$AudioStreamPlayer.stream = laser_sounds
+	timer.timeout.connect(_on_timeout)
+
+func _on_timeout():
+	$AudioStreamPlayer.stop()
+
+	
+func fire() -> void:
+	super()
+	$AudioStreamPlayer.play(offsets[which_offset])
+	timer.start(1.0)
+	which_offset = (which_offset + 1) % 12
 	
